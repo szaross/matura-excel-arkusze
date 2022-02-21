a07f24b33e1c5e431fc6ec2b34c07cddfca1bfac
szaross <szymonszarek02@gmail.com>
2022-02-21 08:07:52 +0100

    .

--- marzec 2021 (p)/zad4/42.py
+++ marzec 2021 (p)/zad4/42.py
@@ -1,27 +1,25 @@
-import os
 galerie = {}
 with open('galerie.txt') as f:
     for line in f.readlines():
         line = line.split(" ")
-        #print("a")
+        # print("a")
         if line[1] not in galerie.keys():
             galerie[line[1]] = {}
-            #print("b")
+            # print("b")
         pole = 0
-        liczba_galerii=0
-        i=2
-        #print("c")
-        while(i!=0):
-            pole = pole + (int(line[i])*int(line[i+1]))
-            liczba_galerii +=1
-            i+=2
-            #print(i)
-            if i==140 or int(line[i+1]) == 0 :
-                i=0
-                
+        liczba_galerii = 0
+        i = 2
+        # print("c")
+        while (i != 0):
+            pole = pole + (int(line[i]) * int(line[i + 1]))
+            liczba_galerii += 1
+            i += 2
+            # print(i)
+            if i == 140 or int(line[i + 1]) == 0:
+                i = 0
 
-        galerie[line[1]]["pole"]=pole
-        galerie[line[1]]["liczba_gal"]=liczba_galerii
+        galerie[line[1]]["pole"] = pole
+        galerie[line[1]]["liczba_gal"] = liczba_galerii
 
 # print(galerie)
 
@@ -30,22 +28,18 @@ with open('galerie.txt') as f:
 #     print(galerie[key]["pole"],end=" ")
 #     print(galerie[key]["liczba_gal"])
 
-minp = galerie[list(galerie.keys())[0]]['pole'] #pole
-minm = list(galerie.keys())[0] #miasto
+minp = galerie[list(galerie.keys())[0]]['pole']  # pole
+minm = list(galerie.keys())[0]  # miasto
 
-maxp = galerie[list(galerie.keys())[0]]['pole'] #pole
-maxm = list(galerie.keys())[0] #miasto
+maxp = galerie[list(galerie.keys())[0]]['pole']  # pole
+maxm = list(galerie.keys())[0]  # miasto
 for key in galerie.keys():
-    if galerie[key]['pole']<minp:
-        minp = galerie[key]['pole'] #pole
-        minm = key #miasto
-    elif galerie[key]['pole']>maxp:
-        maxp = galerie[key]['pole'] #pole
-        maxm = key #miasto
+    if galerie[key]['pole'] < minp:
+        minp = galerie[key]['pole']  # pole
+        minm = key  # miasto
+    elif galerie[key]['pole'] > maxp:
+        maxp = galerie[key]['pole']  # pole
+        maxm = key  # miasto
 
-print(maxm+" "+str(maxp))
-print(minm+" "+str(minp))
-
-
-
-
+print(maxm + " " + str(maxp))
+print(minm + " " + str(minp))
